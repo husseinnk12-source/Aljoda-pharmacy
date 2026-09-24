244ed6e1e60635de9d6838531ac77c9bcd3269e6
husseinnk12-source <hussein.nk12@gmail.com>
2026-09-24 07:29:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -215,6 +215,8 @@ app.patch("/api/admin/orders/:id", auth, (req,res)=>{
   res.json({ok:true});
 });
 
-app.get("*", (req,res) => res.sendFile(path.join(__dirname,"public","index.html")));
+app.get('/{*splat}', (req, res) => {
+  res.sendFile(...);
+});
 
 app.listen(PORT,()=>console.log(`ALJODA Pharmacy running on port ${PORT}`));
